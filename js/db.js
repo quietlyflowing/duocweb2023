3244cf9ef73f859a5698eb1c030efe67122bcd43
Alonso Silva <re.silvas@duocuc.cl>
2023-04-05 01:20:58 -0400

    Cambios al HTML

---
+++ js/db.js
@@ -0,0 +1,14 @@
+let productosTienda = [ 
+{id: '1', nombre: 'Correa Cuadros', precio: 5990, imagen: 'img/correa-cuadros.jpg', categoria: 'gatos'},
+{id: '2', nombre: 'Correa Frutilla', precio: 7990, imagen: 'img/correa-frutilla.jpg', categoria: 'gatos'},
+{id: '3', nombre: 'Correa Flores', precio: 5000, imagen: 'img/correa-flores.jpg', categoria: 'gatos'},
+{id: '4', nombre: 'Bandana Corazones', precio: 3990, imagen: 'img/bandana-corazones.jpg', categoria: 'gatos'},
+{id: '5', nombre: 'Bandana Flores', precio: 3990, imagen: 'img/bandana-flores.jpg', categoria: 'gatos'},
+{id: '6', nombre: 'Bandana Navidad', precio: 3000, imagen: 'img/bandana-navidad.jpg', categoria: 'gatos'},
+{id: '7', nombre: 'Correa Spiderman', precio: 12000, imagen: 'img/correa-spiderman.jpg', categoria: 'perros'}, 
+{id: '8', nombre: 'Correa Avengers', precio: 9990, imagen: 'img/correa-avengers.jpg', categoria: 'perros'}, 
+{id: '9', nombre: 'Correa Minnie', precio: 9990, imagen: 'img/correa-minnie.jpg', categoria: 'perros'}, 
+{id: '10', nombre: 'Bandana Girasoles', precio: 3000, imagen: 'img/bandana-girasoles.jpg', categoria: 'perros'}, 
+{id: '11', nombre: 'Bandana Frutillas', precio: 3500, imagen: 'img/bandana-frutillas.jpg', categoria: 'perros'}, 
+{id: '12', nombre: 'Bandana Marinero', precio: 3000, imagen: 'img/bandana-marinero.jpg', categoria: 'perros'}
+];
