afd9691b801aadae6e3f77a42e5dcd7d283bf19b
Alonso Silva <re.silvas@duocuc.cl>
2023-04-04 21:09:54 -0400

    Más avances

--- js/cart.js
+++ js/cart.js
@@ -1,43 +1,20 @@
-// Initialize the cart with an empty array if it doesn't already exist in localstorage
-if (!localStorage.getItem('cart')) {
-  localStorage.setItem('cart', JSON.stringify([]));
-}
 
-// Add an item to the cart
-function addToCart(item) {
-  // Get the current cart from localstorage
-  let cart = JSON.parse(localStorage.getItem('cart'));
 
-  // Add the new item to the cart
-  cart.push(item);
+let productos = [ 
+{id: '1', nombre: 'Correa Cuadros', precio: 5990, imagen: 'img/correa-cuadros.jpg', categoria: 'gatos'}, 
+{id: '2', nombre: 'Correa Frutilla', precio: 7990, imagen: 'img/correa-frutilla.jpg', categoria: 'gatos'}, 
+{id: '3', nombre: 'Correa Flores', precio: 5000, imagen: 'img/correa-flores.jpg', categoria: 'gatos'}, 
+{id: '4', nombre: 'Bandana Corazones', precio: 3990, imagen: 'img/bandana-corazones.jpg', categoria: 'gatos'}, 
+{id: '5', nombre: 'Bandana Flores', precio: 3990, imagen: 'img/bandana-flores.jpg', categoria: 'gatos'}, 
+{id: '6', nombre: 'Bandana Navidad', precio: 3000, imagen: 'img/bandana-navidad.jpg', categoria: 'gatos'},
+{id: '7', nombre: 'Correa Spiderman', precio: 12000, imagen: 'img/correa-spiderman.jpg', categoria: 'perros'}, 
+{id: '8', nombre: 'Correa Avengers', precio: 9990, imagen: 'img/correa-avengers.jpg', categoria: 'perros'}, 
+{id: '9', nombre: 'Correa Minnie', precio: 9990, imagen: 'img/correa-minnie.jpg', categoria: 'perros'}, 
+{id: '10', nombre: 'Bandana Girasoles', precio: 3000, imagen: 'img/bandana-girasoles.jpg', categoria: 'perros'}, 
+{ id: '11', nombre: 'Bandana Frutillas', precio: 3500, imagen: 'img/bandana-frutillas.jpg', categoria: 'perros'}, 
+{id: '12', nombre: 'Bandana Marinero', precio: 3000, imagen: 'img/bandana-marinero.jpg', categoria: 'perros'}
+] 
 
-  // Save the updated cart back to localstorage
-  localStorage.setItem('cart', JSON.stringify(cart));
-}
 
-// Remove an item from the cart
-function removeFromCart(item) {
-  // Get the current cart from localstorage
-  let cart = JSON.parse(localStorage.getItem('cart'));
 
-  // Find the index of the item to remove
-  const index = cart.findIndex((cartItem) => cartItem.id === item.id);
-
-  // Remove the item from the cart
-  cart.splice(index, 1);
-
-  // Save the updated cart back to localstorage
-  localStorage.setItem('cart', JSON.stringify(cart));
-}
-
-// Get the current cart
-function getCart() {
-  // Get the current cart from localstorage
-  return JSON.parse(localStorage.getItem('cart'));
-}
-
-// Clear the cart
-function clearCart() {
-  // Remove the cart from localstorage
-  localStorage.removeItem('cart');
-}
+//let productoGatos [];
---
+++ js/misc.js
@@ -0,0 +1,9 @@
+function showCurrentYearFooter() {
+//Muestra la fecha actual en el documento HTML
+//Esto perfectamente podría estar suelto, pero meh.
+let currentYear = new Date().getFullYear();
+document.getElementById("footer").innerHTML = `<div class="container text-center">
+<span class="text-muted">Copyright © ${currentYear} TiendaPET® Ltda.</span>
+</div>`}
+
+showCurrentYearFooter();
